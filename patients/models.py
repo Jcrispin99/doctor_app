78bbe8d88338572f0d6ba1c5332cddb83f30f34c
Panchito <j99crispin@gmail.com>
2025-05-17 12:13:36 -0500

    first commit

---
+++ patients/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from .models import Doctor
+# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    contact_number  = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.TextField()
+    medical_history = models.TextField()
+
+class Insurance(models.Model):
+    patient = models.ForeignKey(Patient, related_name='insurances', on_delete=models.CASCADE)
+    provider = models.CharField(max_length=100)
+    policy_number = models.CharField(max_length=50)
+    expliration_date = models.DateField()
+
+
+class MedicalRecord(models.Model):
+    patient = models.ForeignKey(Patient, related_name='medical_records', on_delete=models.CASCADE)
+    date = models.DateField()
+    diagnosis = models.TextField()
+    treatment = models.TextField()
+    follow_up_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.patient}'s Medical Record"
