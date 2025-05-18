5b0193753eb39d9fde57c259fef365ea08218194
Panchito <j99crispin@gmail.com>
2025-05-18 17:56:07 -0500

    Crud Views completo

--- patients/views.py
+++ patients/views.py
@@ -5,7 +5,7 @@ from .models import Patient
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView # depende de lo que requiera en la API
 
 class ListPatientsView(ListAPIView, CreateAPIView):#explicame que pasa de fondo ya que no entiendo
     #al importar ListAPIView y CreateAPIView, se puede usar el metodo get y post cosa que simplifica más el codigo
@@ -20,30 +20,7 @@ class ListPatientsView(ListAPIView, CreateAPIView):#explicame que pasa de fondo 
     #     serializer.save()
     #     return Response(status=status.HTTP_201_CREATED)
 
-class PatientDetailView(APIView):
+class PatientDetailView(RetrieveUpdateDestroyAPIView):
     allowed_methods = ['GET', 'PUT', 'DELETE']
-    def get(self, request, pk):
-        try:
-            patient = Patient.objects.get(pk=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = PatientSerializer(patient)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        try:
-            patient = Patient.objects.get(pk=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = PatientSerializer(patient, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK) 
-
-    def delete(self, request, pk):
-        try:
-            patient = Patient.objects.get(pk=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        patient.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
